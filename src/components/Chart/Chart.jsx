d9016086c64ce9a9db698ca08e4e3fbe11e53396
Kristina <kristy.tabak@gmail.com>
2023-05-29 00:46:52 +0200

    Added check for chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,11 +1,16 @@
 const Chart = ({ total, switchOrder }) => {
+  // Checks if the total value is greater than 100.
+  // The total value is set to 100 to ensure the bar does not exceed 100%.
+  if (total > 100) {
+    total = 100;
+  }
   return (
     <div
       data-testid="chart"
       style={{
         backgroundColor: `${switchOrder ? "#205c4e" : "#813d3a"}`,
         height: "1.250em",
-        width: `${total > 100 ? 100 : total}%`,
+        width: `${total}%`,
         position: "relative",
         top: 20,
         left: `${switchOrder ? `${100 - total}%` : 0}`,
